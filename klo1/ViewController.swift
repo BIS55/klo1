d5439aa778efa1ffeba9a13886954cfdde3c3f87
Иван Беляев <bis55@icloud.com>
2018-11-28 12:45:01 +0600

    Initial Commit

---
+++ klo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  klo1
+//
+//  Created by Ivan Belyaev on 28/11/2018.
+//  Copyright © 2018 Ivan Belyaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
